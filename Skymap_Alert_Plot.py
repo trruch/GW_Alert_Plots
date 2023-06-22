d2d212137067c8f3e143053f429d419dc4890459
Thomas Ruch <98111019+trruch@users.noreply.github.com>
2023-06-22 15:36:45 -0400

    Add script for generating skymap.

---
+++ Skymap_Alert_Plot.py
@@ -0,0 +1,130 @@
+import os
+from astropy.coordinates import SkyCoord
+from astropy.io import fits
+from astropy import units as u
+import ligo.skymap.plot
+from matplotlib import pyplot as plt
+import healpy as hp
+import numpy as np
+import ligo.skymap
+
+
+def make_alert_skymap(map_path):
+    skymap = hp.read_map(map_path, field=None)
+    prob, distmu, distsigma, distnorm = skymap
+    
+    npix = len(prob)
+    nside = hp.npix2nside(npix)
+
+
+    maxprobcoord_tup = hp.pix2ang(nside, np.argmax(prob))
+    maxprobcoord = [0, 0]
+    maxprobcoord[1] = np.rad2deg(0.5*np.pi-maxprobcoord_tup[0])
+    maxprobcoord[0] = np.rad2deg(maxprobcoord_tup[1])
+    
+
+    def find_area(prob_array, contours, nside = nside):
+        areas = []
+        for contour in contours:
+            sortedprob = np.sort(prob_array)
+            probsum = 0
+            probcutoff = 1
+            npix = 0
+            while probsum<contour:
+                probsum = probsum+sortedprob[-1]
+                probcutoff = sortedprob[-1]
+                sortedprob = sortedprob[:-1]
+                npix = npix+1
+
+            area = npix * hp.nside2pixarea(nside, degrees=True)
+            areas.append(area)
+        return ([int(round(i,0)) for i in areas])
+    
+    
+    def ci(level, sorted_prob=np.flip(np.sort(prob))):
+        csum = 0
+        c = 0
+        index = 0
+        while csum < level:
+            csum += sorted_prob[index]
+            c = sorted_prob[index]
+            index += 1
+        return c
+
+    c90 = ci(.5)
+    c50 = ci(.9)
+    levels = [c50, c90]
+        
+    area50, area90 = find_area(prob, contours = [.5, .9])
+    
+    maxprob_ra = round(maxprobcoord[0],2)
+    maxprob_dec = round(maxprobcoord[1],2)
+    
+    maxprob_dist = int(round(distmu[np.argmax(prob)],0))
+    maxprob_distsigma = int(round(distsigma[np.argmax(prob)],0))
+    
+    return (area50, area90, maxprob_ra, maxprob_dec, maxprob_dist, maxprob_distsigma, levels)
+
+if __name__ == "__main__":
+    
+    url = input('Url: ')
+    name = input('Event Name: ')
+
+    area50, area90, maxprob_ra, maxprob_dec, maxprob_dist, maxprob_distsigma, levels = make_alert_skymap(url)
+
+
+
+    center = SkyCoord(maxprob_ra, maxprob_dec, unit="deg")  # defaults to ICRS frame
+
+
+    fig = plt.figure(figsize=(10, 10), dpi=100)
+    plt.annotate('Event Name: {}'.format(name) + '\n'
+                 + r'50% Area: {} deg$^2$'.format(area50) + '\n'
+                 + r'90% Area: {} deg$^2$'.format(area90) + '\n'
+                 + r'Max Prob Coordinates: ({},{})'.format(maxprob_ra, maxprob_dec) + '\n'
+                 + r'Max Prob Distance: {}$\pm${} Mpc'.format(maxprob_dist, maxprob_distsigma)
+                 ,(0.9,0.8))
+    plt.box(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    ax = plt.axes(
+        #[0.05, 0.05, 0.9, 0.9],
+        projection='astro hours mollweide')
+
+    ax_inset = plt.axes(
+        [0.9, 0.2, 0.2, 0.2],
+        projection='astro zoom',
+        center=center,
+        radius=10*u.deg)
+
+    for key in ['ra', 'dec']:
+        ax_inset.coords[key].set_ticklabel_visible(True)
+        ax_inset.coords[key].set_ticks_visible(True)
+    ax.grid()
+    ax_inset.grid()
+    ax.mark_inset_axes(ax_inset)
+    ax.connect_inset_axes(ax_inset, 'upper right')
+    ax.connect_inset_axes(ax_inset, 'lower left')
+    ax_inset.scalebar((0.1, 0.1), 5 * u.deg).label()
+
+    ax.imshow_hpx(url, cmap='cylon')
+    cs = ax.contour_hpx(url, levels = levels, colors = ['black'], linewidths = [1,0.5])
+    ct = ax_inset.contour_hpx(url, levels = levels, colors = ['black'], linewidths = [1,0.5])
+
+    ax_inset.imshow_hpx(url, cmap='cylon')
+    ax_inset.plot(
+        maxprob_ra, maxprob_dec,
+        transform=ax_inset.get_transform('world'),
+        marker=ligo.skymap.plot.reticle(),
+        markersize=30,
+        markeredgewidth=3)
+    ax.plot(
+        maxprob_ra, maxprob_dec,
+        transform=ax.get_transform('world'),
+        marker=ligo.skymap.plot.reticle(inner=0),
+        markersize=10,
+        markeredgewidth=3)
+    
+    plt.savefig('/Users/thomasruch/UMICH/O4testing/'+name,dpi=300, bbox_inches = "tight")
+   
