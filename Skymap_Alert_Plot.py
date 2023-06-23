7b2d3e1c310b6814e70fceb8200e545a40460638
Thomas Ruch <98111019+trruch@users.noreply.github.com>
2023-06-23 09:23:07 -0400

    Import modules

--- Skymap_Alert_Plot.py
+++ Skymap_Alert_Plot.py
@@ -7,6 +7,13 @@ from matplotlib import pyplot as plt
 import healpy as hp
 import numpy as np
 import ligo.skymap
+import pandas as pd
+from astroplan import Observer, FixedTarget
+from astroplan import (AltitudeConstraint, AirmassConstraint,AtNightConstraint)
+from astroplan import is_observable
+from astropy.time import Time
+from astroplan.plots import plot_airmass
+import sys
 
 
 def make_alert_skymap(map_path):
